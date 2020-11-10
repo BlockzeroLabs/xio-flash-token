d0c456720c44a8b950d2673322c79dd6ed4bf2b2
shakeib98 <shakeib98@outlook.com>
2020-11-10 20:38:09 +0500

    flash token completed

---
+++ scripts/deploy.ts
@@ -0,0 +1,32 @@
+// We require the Buidler Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+// When running the script with `buidler run <script>` you'll find the Buidler
+// Runtime Environment's members available in the global scope.
+import { ethers } from "@nomiclabs/buidler";
+import { Contract, ContractFactory } from "ethers";
+
+async function main(): Promise<void> {
+  // Buidler always runs the compile task when running scripts through it.
+  // If this runs in a standalone fashion you may want to call compile manually
+  // to make sure everything is compiled
+  // await run("compile");
+
+  const INITIAL_MINTER = "0xDE174710543dCED471A5747Fe3060d11E881a356";
+
+  // We get the contract to deploy
+  const FlashToken: ContractFactory = await ethers.getContractFactory("FlashToken");
+  console.log(`Start deploying Flash Token with initial minter: ${INITIAL_MINTER}`);
+  const flashToken: Contract = await FlashToken.deploy(INITIAL_MINTER);
+  await flashToken.deployed();
+
+  console.log("Flash Token deployed to: ", flashToken.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
